778d8ec7c87910e4d21ac98ad400a95acb0dd9a6
nodet <noodet@gmail.com>
2018-11-20 17:38:56 +0100

    añadiendo listado de usuarios conectados

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -7,6 +7,22 @@ const $messageForm= $('#message-form');
 const $messageBox= $('#message');
 const $chat= $('#chat');
 
+//objetos dl nickname
+const $nickForm = $('#nickForm');
+const $nickname = $('#nickname');
+const $nickError = $('#nickError');
+const $users = $('#usernames');
+
+$nickForm.submit(e => {
+    e.preventDefault();
+    console.log("enviadando le nick");
+    socket.emit('new user', $nickname.val(), data => {
+
+    });
+
+})
+
+
 //eventos
 $messageForm.submit( e =>  {
     e.preventDefault();
--- src/sockets.js
+++ src/sockets.js
@@ -1,8 +1,27 @@
 
 module.exports = function (io) {
 
+let nicknames = [
+];
+
     io.on('connection', socket => {
         console.log('new user connected');
+
+
+        socket.on('new user', (data,cb) => {
+            console.log(data);
+            if (nicknames.indexOf(data) != -1){
+                cb(false);
+            } else{
+                cb(true);
+                socket.nickname = data;
+                nicknames.push(sockeet.nickname);
+            }
+    
+        });
+
+
+
 
         socket.on('send message', function(data){
             console.log(data);
